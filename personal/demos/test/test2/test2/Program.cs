77d413b78bc0a45be5e2e5862c4e942b884b47a3
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-02-03 10:24:55 +0200

    Commit another test file

---
+++ personal/demos/test/test2/test2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's do another test now...");
+            for (int i = 0; i < 10; ++i)
+            {
+                Console.WriteLine(i);
+            }
+
+            int x = 10;
+            int y = 200;
+            int result = x + y + 50;
+            Console.WriteLine("Result is: " + result);
+        }
+    }
+}

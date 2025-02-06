28b58d5b8cc7f85fec7ed935bb396d7c204d35df
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-02-06 17:11:59 +0200

    Add partial class demo

---
+++ personal/demos/oop/partialClasses/partialClasses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+// Partial classes
+public partial class Book
+{
+    public String Title { get; set; }
+    public String Author { get; set; }
+    public int Year { get; set; }
+}
+
+public partial class Book
+{
+    public Book(String title, String author, int year)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Title: {Title}, Author: {Author}, Year: {Year}");
+    }
+}
+
+namespace partialClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("Les Intouchables", "Romain Garry", 1994);
+
+            b1.ShowInfo();
+        }
+    }
+}

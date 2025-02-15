c04965857c20b943ed854c524aa54157ac257bc3
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-02-15 16:51:48 +0200

    Add more demos

--- personal/demos/advanced/lesson01/lesson01/Program.cs
+++ personal/demos/advanced/lesson01/lesson01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lesson01
 {
@@ -72,12 +73,62 @@ namespace lesson01
             }
         }
 
+        // 03. 
+        class Blegh
+        {
+            private readonly string _prefix;
+
+            public Blegh(string prefix)
+            {
+                _prefix = prefix;
+            }
+
+            public void DoStuff(string output)
+            {
+                Console.WriteLine($"From Blegh {_prefix} -> {output}");
+            }
+        }
+
+        // 04.
+        delegate bool IntPredicate(int number);
+
+        static IEnumerable<int> Filter(IEnumerable<int> source, IntPredicate predicate)
+        {
+            List<int> list = new List<int>();
+
+            foreach(int item in source)
+            {
+                if (predicate(item))
+                    list.Add(item);
+            }
+
+            return list;
+        }
+
+        static bool IsMod3(int number)
+        {
+            return number % 3 == 0;
+        }
+
         static void Main(string[] args)
         {
             // FizzBuzz fb = new FizzBuzz(new ConsoleFizzOutput());
             // fb.Run(1, 100);
 
-            Run(WriteFizzBuzz, 1, 100);
+            // Run(WriteFizzBuzz, 1, 100);
+
+            // Blegh b1 = new Blegh("Blegh 1");
+            // Blegh b2 = new Blegh("Blegh 2");
+
+            // Run(b1.DoStuff, 1, 10);
+            // Run(b2.DoStuff, 1, 10);
+
+            var arr = new[] { 1, 2, 3, 4, 5, 6 };
+            var filteredList = Filter(arr, IsMod3);
+            foreach (int item in filteredList)
+            {
+                Console.WriteLine(item);
+            }
 
             Console.ReadKey();
         }

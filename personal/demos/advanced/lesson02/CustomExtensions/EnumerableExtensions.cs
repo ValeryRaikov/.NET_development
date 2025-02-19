4aa6bce975820a38804ede36b384209d76fcf518
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-02-19 16:26:32 +0200

    Add more complex tasks

---
+++ personal/demos/advanced/lesson02/CustomExtensions/EnumerableExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomExtensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> projection)
+        {
+            foreach (var item in source)
+            {
+                yield return projection(item);
+            }
+        }
+
+        public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static TSource MyFirst<TSource>(this IEnumerable<TSource> source)
+        {
+            foreach (var item in source)
+            {
+                return item;
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        public static TSource MyFirst<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        public static bool MyAll<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (!predicate(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool MyAny<TSource>(this IEnumerable<TSource> source)
+        {
+            foreach (var item in source)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool MyAny<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ personal/demos/advanced/lesson02/CustomExtensions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("MySelect (Multiply by 2):");
+            foreach (var num in numbers.MySelect(n => n * 2))
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("MyWhere (Only Even Numbers):");
+            foreach (var num in numbers.MyWhere(n => n % 2 == 0))
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine($"MyFirst: {numbers.MyFirst()}");
+
+            Console.WriteLine($"MyFirst (Greater than 3): {numbers.MyFirst(n => n > 3)}");
+
+            Console.WriteLine($"MyAll (All > 0): {numbers.MyAll(n => n > 0)}");
+            Console.WriteLine($"MyAll (All > 3): {numbers.MyAll(n => n > 3)}");
+
+            Console.WriteLine($"MyAny (Any Elements Exist): {numbers.MyAny()}");
+            Console.WriteLine($"MyAny (Any Even Numbers): {numbers.MyAny(n => n % 2 == 0)}");
+            Console.WriteLine($"MyAny (Any > 10): {numbers.MyAny(n => n > 10)}");
+        }
+    }
+}

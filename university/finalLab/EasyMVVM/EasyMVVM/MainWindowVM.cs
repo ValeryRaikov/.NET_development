537b4493981964833b0a7018fb965dc323b1884e
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-02-28 16:34:39 +0200

    Add MVVM project

---
+++ university/finalLab/EasyMVVM/EasyMVVM/MainWindowVM.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace EasyMVVM
+{
+    public class MainWindowVM : DependencyObject, INotifyPropertyChanged
+    {
+        //This event will be fired to notify any listeners of a change in a property value.
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //set up a private class varialbe that holds the value of the _Backing Property
+        private ObservableCollection<string> _BackingProperty;
+
+        //This is the publically viewable Property for this class
+        public ObservableCollection<string> BoundProperty
+        {
+            get { return _BackingProperty; }
+            set { _BackingProperty = value; PropChanged("BoundProperty"); }
+        }
+
+        public MainWindowVM()
+        {
+            Model m = new Model();
+            BoundProperty = m.GetData();
+        }
+
+        //Tell WPF Binding that this property value has changed
+        public void PropChanged(string propertyName)
+        {
+            //Did WPF registerd to listen to this event
+            //Tell WPF that this property changed
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

55f15c536afda44789a5be3c5e8fdebc0f227ba0
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-03-01 20:37:14 +0200

    Add EmployeeViewModel logic

--- personal/demos/MVVM/MVVMDemo/MVVMDemo/MainWindow.xaml.cs
+++ personal/demos/MVVM/MVVMDemo/MVVMDemo/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MVVMDemo.ViewModels;
 
 namespace MVVMDemo
 {
@@ -20,9 +21,14 @@ namespace MVVMDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        EmployeeViewModel ViewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ViewModel = new EmployeeViewModel();
+            this.DataContext = ViewModel;
         }
     }
 }
---
+++ personal/demos/MVVM/MVVMDemo/MVVMDemo/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using MVVMDemo.Models;
+
+namespace MVVMDemo.ViewModels
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<Employee> _employeesList;
+
+        public EmployeeService EmployeeService { get; }
+
+        public List<Employee> EmployeesList
+        {
+            get { return _employeesList; }
+            set 
+            { 
+                _employeesList = value;
+                OnPropertyChanged("EmployeesList");
+            }
+        }
+
+        public EmployeeViewModel()
+        {
+            EmployeeService = new EmployeeService();
+            LoadData();
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void LoadData()
+        {
+            EmployeesList = EmployeeService.GetAllEmployees();
+        }
+    }
+}

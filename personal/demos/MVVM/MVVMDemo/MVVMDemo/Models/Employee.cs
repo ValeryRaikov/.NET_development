30b3e764c556efcb525b2ddd379201af809fc847
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-03-01 19:59:18 +0200

    Add Models

---
+++ personal/demos/MVVM/MVVMDemo/MVVMDemo/Models/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+
+namespace MVVMDemo.Models
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _id;
+        private string _name;
+        private int _age;
+
+        public int Id
+        {
+            get { return _id; }
+            set 
+            { 
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set 
+            { 
+                _age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        public Employee(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ personal/demos/MVVM/MVVMDemo/MVVMDemo/Models/EmployeeService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVVMDemo.Models
+{
+    public class EmployeeService
+    {
+        private static List<Employee> EmployeesList;
+
+        public EmployeeService()
+        {
+            EmployeesList = new List<Employee>()
+            {
+                new Employee(1, "Valery", 21),
+            };
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return EmployeesList;
+        }
+
+        public bool AddEmployee(Employee newEmp)
+        {
+            if (EmployeesList.Contains(newEmp))
+                return false;
+
+            if (newEmp.Age < 18 || newEmp.Age > 66)
+                throw new ArgumentException("Invalid age limit for employee!");
+
+            EmployeesList.Add(newEmp);
+
+            return true;
+        }
+
+        public bool UpdateEmployee(Employee empToUpdate)
+        {
+            bool isUpdated = false;
+            Employee foundEmp = EmployeesList.FirstOrDefault(e => e.Id == empToUpdate.Id);
+
+            if (foundEmp != null)
+            {
+                foundEmp.Name = empToUpdate.Name;
+                foundEmp.Age = empToUpdate.Age;
+                isUpdated = true;
+            }
+
+            return isUpdated;
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+            bool isDeleted = false;
+            Employee foundEmp = EmployeesList.FirstOrDefault(e => e.Id == id);
+
+            if (foundEmp != null)
+            {
+                EmployeesList.Remove(foundEmp);
+                isDeleted = true;
+            }
+
+            return isDeleted;
+        }
+
+        public Employee SearchEmployee(int id)
+        {
+            return EmployeesList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}

18b33396567ddd60e43c5fc81fd341366dd08074
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-03-24 22:09:05 +0200

    Create project following microsoft docs

---
+++ docs/Maui/Notes/Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+namespace Notes.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMoreBtn_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.About about)
+        {
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
+}

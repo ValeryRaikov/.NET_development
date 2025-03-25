bdfacc7356af14afdeaa21c2ce08e1a844327880
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-03-25 10:25:45 +0200

    Implement MVVM principles

--- docs/Maui/Notes/Notes/Views/AboutPage.xaml.cs
+++ docs/Maui/Notes/Notes/Views/AboutPage.xaml.cs
@@ -6,12 +6,4 @@ public partial class AboutPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
-    private async void LearnMoreBtn_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.About about)
-        {
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
-    }
 }

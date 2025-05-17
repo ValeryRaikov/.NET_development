ed892ec777ab8825b7f9e512fc80b9c763f8cb67
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-05-17 13:24:37 +0300

    Add demos

---
+++ examPrep/Delegates/Del1/Del1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Del1
+{
+    public delegate void PrintMessage(string message); // Delegate
+
+    internal class Program
+    {
+        static void Main()
+        {
+            PrintMessage printer = PrintToConsole;
+            printer("Hello, Delegates!"); // Method Call
+
+            printer += PrintToFile;
+            printer("This goes to console and file!");
+
+            printer += PrintAgain;
+            printer("Damn that is cool...");
+
+            printer += (string msg) => Console.WriteLine($"Anonymous function: {msg}");
+            printer("Hello from Lambda!");
+
+            // printer -= PrintToConsole;
+            // printer -= PrintToFile;
+            // printer -= PrintAgain;
+        }
+
+        public static void PrintToConsole(string msg) => 
+            Console.WriteLine($"Console: {msg}");
+
+        public static void PrintToFile(string msg) => 
+            File.WriteAllText("output.txt", $"File: {msg}");
+
+        public static void PrintAgain(string msg) =>
+            Console.WriteLine($"Again: {msg}");
+    }
+}

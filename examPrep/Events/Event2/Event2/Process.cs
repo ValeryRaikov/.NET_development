3865883552e70c59bc9236f08f95587e4e4fa6f6
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-05-17 13:39:30 +0300

    Add demos

---
+++ examPrep/Events/Event2/Event2/Process.cs
@@ -0,0 +1,19 @@
+﻿namespace Event2
+{
+    public class Process
+    {
+        public event EventHandler? ProcessCompleted; 
+
+        public void Start()
+        {
+            Console.WriteLine("Processing...");
+            Thread.Sleep(1000);
+            OnProcessCompleted();
+        }
+
+        protected virtual void OnProcessCompleted()
+        {
+            ProcessCompleted?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ examPrep/Events/Event2/Event2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Event2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            process.ProcessCompleted += ProcessCompletedHandler;
+            process.Start();
+            process.ProcessCompleted -= ProcessCompletedHandler;
+
+            process.ProcessCompleted += Another;
+            process.Start();
+        }
+
+        static void ProcessCompletedHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Event received: Process completed!");
+        }
+
+        static void Another(object sender, EventArgs e)
+        {
+            Console.WriteLine("Testing some crazy stuff...");
+        }
+    }
+}

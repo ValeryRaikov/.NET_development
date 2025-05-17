3174a3b47f6d5f43d19e3f0b706fe47e0995cb9b
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-05-17 13:31:44 +0300

    Add demos

---
+++ examPrep/Events/Event1/Event1/Button.cs
@@ -0,0 +1,13 @@
+﻿namespace Event1
+{
+    internal class Button
+    {
+        public event EventHandler? Clicked;
+
+        public void Click()
+        {
+            Console.WriteLine("Button was clicked!");
+            Clicked?.Invoke(this, EventArgs.Empty); 
+        }
+    }
+}
---
+++ examPrep/Events/Event1/Event1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Event1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Button button = new Button();
+
+            button.Clicked += OnButtonClicked;
+            button.Clicked += (object? sender, EventArgs e) => 
+                Console.WriteLine("Did it again from Lambda...");
+
+            button.Click();
+        }
+
+        static void OnButtonClicked(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Someone clicked the button!");
+        }
+    }
+}

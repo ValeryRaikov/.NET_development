9a992d97629f535292223a81cfd9eb647537cf3a
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-05-17 13:34:36 +0300

    Add demos

---
+++ examPrep/Delegates/Del2/Del2/Process.cs
@@ -0,0 +1,18 @@
+﻿namespace Del2
+{
+    public delegate void Notify();
+
+    public class Process
+    {
+        public Notify? OnProcessCompleted;
+
+        public void Start()
+        {
+            Console.WriteLine("Processing...");
+            Thread.Sleep(1000);
+
+            OnProcessCompleted?.Invoke();
+        }
+    }
+
+}
---
+++ examPrep/Delegates/Del2/Del2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Del2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            process.OnProcessCompleted = NotifyUser;
+            process.Start();
+        }
+
+        static void NotifyUser()
+        {
+            Console.WriteLine("Process completed!");
+        }
+    }
+}
